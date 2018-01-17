6106abd6764b5c6956a18b497cfa39e0f4a35679
Doris Ngo Momha <dorismha@icloud.com>
2018-01-17 23:07:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,4 @@ function sayHiToGrandma(string) {
     return ("I can\'t hear you!")
   } else if (string.toUpperCase() === string) {
     return ("YES INDEED!")
-  }
-}
-function sayHiToGrandma(string) {
-  if (string === "I love you, Grandma.") {
-    return ("I love you, too.")
-  }
-}
+  } if (string === "I love you, gran")
