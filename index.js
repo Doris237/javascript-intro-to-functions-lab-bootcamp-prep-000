8c6ea7e2d7483b0e9be1aa18654fe1a6fa6d216e
Doris Ngo Momha <dorismha@icloud.com>
2018-01-17 22:48:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,4 +13,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return ("I can\'t hear you")
-  } else 
+  } else if (string.toUpperCase() === string) {
+    return ("YES INDEED")
+  }
+}
